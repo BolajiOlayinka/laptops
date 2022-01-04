ce1613e6dc6cd6d9b70745f0d7f801e18808845e
Bolaji Olayinka <bolajiolayinka980@gmail.com>
2022-01-04 21:09:01 +0100

    testing with mocha

---
+++ index.js
@@ -0,0 +1,47 @@
+const laptopBrands= require('./laptops.json');
+
+function _lower(input) {
+    return input.toLowerCase().trim()
+}
+
+module.exports = {
+    all: function() {
+        return laptopBrands;
+    },
+    states: function () {
+        return statesAndLocalGov.map(function (nigeriaStates) {
+            return nigeriaStates.state;
+        });
+    },
+    senatorial_districts: function (state) {
+        state = _lower(state);
+
+        if (!state || state == "") {
+            throw new Error('Invalid Nigeria State');
+        }
+
+        if (['fct', 'f.c.t', 'abuja', 'f c t'].includes(state)) {
+            state = 'Federal Capital Territory'
+        }
+
+        const response = statesAndLocalGov.find(function (nigeriaStates) {
+            return _lower(nigeriaStates.state) === _lower(state);
+        });
+        return response.senatorial_districts;
+    },
+    lgas: function (state) {
+        state = _lower(state);
+
+        if (!state || state == "") {
+            throw new Error('Invalid Nigeria State');
+        }
+
+        if (['fct', 'f.c.t', 'abuja', 'f c t'].includes(state)) {
+            state = 'Federal Capital Territory'
+        }
+
+        return statesAndLocalGov.find(function (nigeriaStates) {
+            return _lower(nigeriaStates.state) === _lower(state);
+        });
+    }
+};
