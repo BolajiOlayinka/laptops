904d22c1d8cd98e915c00a805889681678a167a2
Bolaji Olayinka <bolajiolayinka980@gmail.com>
2022-01-06 19:04:16 +0100

    formatting

--- index.js
+++ index.js
@@ -1,90 +1,90 @@
-const laptopBrands= require('./laptops.json');
+const laptopBrands = require("./laptops.json");
 
 function _lower(input) {
-    return input.toLowerCase().trim()
+  return input.toLowerCase().trim();
 }
 
 module.exports = {
-    getAll: function() {
-        return laptopBrands;
-    },
-    getBrands: function () {
-        // return laptopBrands.map(function (laptop) {
-        //     return laptop
-        // });
-        return laptopBrands[0]['models']
-    },
-    getBrand: function (param) {
-        const isBrand = isNaN(param);
-        const queryType = isBrand ? 'brands' : 'models';
-        return laptopBrands.find(function(laptop) {
-          if (laptop[queryType] === param) {
-            return laptop;
-          }
-        })
-      },
-    getModel:function(){
-        return laptopBrands.map(function(laptop){
-            // Remove first and second array
-            let newArr =[];
-            newArr.push(laptop.models)
-            return newArr.flat()
-            // let newArr = arr.shift();
-            // return newArr.concat.apply();
-        })
-    },
-          getBrand: function (param) {
-          const isBrand = isNaN(param);
-          const queryType = isBrand ? 'brands' : 'code';
-          return laptopBrands.find(function(laptop) {
-            if (laptop[queryType] === param) {
-              return laptop;
-            }
-          })
-        },
-    // module.exports = {
-    //     getBanks: function() {
-    //       return banks;
-    //     },
-      
-    //     getModel: function (param) {
-    //       const isModel = isNaN(param);
-    //       const queryType = isModel ? 'models' : 'code';
-    //       return laptopBrands.find(function(laptop) {
-    //         if (laptop[queryType] === param) {
-    //           return laptop;
-    //         }
-    //       })
-    //     }
+  getAll: function () {
+    return laptopBrands;
+  },
+  getBrands: function () {
+    // return laptopBrands.map(function (laptop) {
+    //     return laptop
+    // });
+    return laptopBrands[0]["models"];
+  },
+  getBrand: function (param) {
+    const isBrand = isNaN(param);
+    const queryType = isBrand ? "brands" : "models";
+    return laptopBrands.find(function (laptop) {
+      if (laptop[queryType] === param) {
+        return laptop;
+      }
+    });
+  },
+  getModel: function () {
+    return laptopBrands.map(function (laptop) {
+      // Remove first and second array
+      let newArr = [];
+      newArr.push(laptop.models);
+      return newArr.flat();
+      // let newArr = arr.shift();
+      // return newArr.concat.apply();
+    });
+  },
+  getBrand: function (param) {
+    const isBrand = isNaN(param);
+    const queryType = isBrand ? "brands" : "code";
+    return laptopBrands.find(function (laptop) {
+      if (laptop[queryType] === param) {
+        return laptop;
+      }
+    });
+  },
+  // module.exports = {
+  //     getBanks: function() {
+  //       return banks;
+  //     },
 
-    senatorial_districts: function (state) {
-        state = _lower(state);
-        if (!state || state == "") {
-            throw new Error('Invalid Nigeria State');
-        }
+  //     getModel: function (param) {
+  //       const isModel = isNaN(param);
+  //       const queryType = isModel ? 'models' : 'code';
+  //       return laptopBrands.find(function(laptop) {
+  //         if (laptop[queryType] === param) {
+  //           return laptop;
+  //         }
+  //       })
+  //     }
 
-        if (['fct', 'f.c.t', 'abuja', 'f c t'].includes(state)) {
-            state = 'Federal Capital Territory'
-        }
-
-        const response = statesAndLocalGov.find(function (nigeriaStates) {
-            return _lower(nigeriaStates.state) === _lower(state);
-        });
-        return response.senatorial_districts;
-    },
-    lgas: function (state) {
-        state = _lower(state);
-
-        if (!state || state == "") {
-            throw new Error('Invalid Nigeria State');
-        }
-
-        if (['fct', 'f.c.t', 'abuja', 'f c t'].includes(state)) {
-            state = 'Federal Capital Territory'
-        }
-
-        return statesAndLocalGov.find(function (nigeriaStates) {
-            return _lower(nigeriaStates.state) === _lower(state);
-        });
+  senatorial_districts: function (state) {
+    state = _lower(state);
+    if (!state || state == "") {
+      throw new Error("Invalid Nigeria State");
     }
+
+    if (["fct", "f.c.t", "abuja", "f c t"].includes(state)) {
+      state = "Federal Capital Territory";
+    }
+
+    const response = statesAndLocalGov.find(function (nigeriaStates) {
+      return _lower(nigeriaStates.state) === _lower(state);
+    });
+    return response.senatorial_districts;
+  },
+  lgas: function (state) {
+    state = _lower(state);
+
+    if (!state || state == "") {
+      throw new Error("Invalid Nigeria State");
+    }
+
+    if (["fct", "f.c.t", "abuja", "f c t"].includes(state)) {
+      state = "Federal Capital Territory";
+    }
+
+    return statesAndLocalGov.find(function (nigeriaStates) {
+      return _lower(nigeriaStates.state) === _lower(state);
+    });
+  },
 };
--- test/test.js
+++ test/test.js
@@ -1,55 +1,50 @@
-'use strict';
+"use strict";
 
-var assert = require('assert');
-var laptops = require('../index');
+var assert = require("assert");
+var laptops = require("../index");
 // var statesAndLocalGov = require('../src/statesAndLocalGov.json')
 
+describe("laptops", function () {
+  it("#all()", function () {
+    var response = laptops.getAll();
 
-describe('laptops', function() {
-    it('#all()', function() {
-        var response = laptops.getAll();
-        
-        // console.log(response[0]);
-        // assert.equal(response.length, 37);
-        // assert.equal(typeof response toequal[]);
-        // assert.equal(response[1].state, statesAndLocalGov[1].state);
-        // assert.equal(response[1].lgas.length, statesAndLocalGov[1].lgas.length);
-    });
-    it('#brands()', function() {
-        var response = laptops.getBrands()
-        // console.log(response)
-        // assert.equal(response.length, 37);
-        // assert.equal(typeof response, typeof []);
-        // assert.equal(response[0], 'Apple');
-    });
-    it('should get laptop details', function() {
-        var response = laptops.getBrand('Hp');
-        console.log(response)
-        
-      });
-     it('#models()', function() {
-        var response = laptops.getModel()
-        // console.log(response);
-        // console.log(response)
-        // assert.equal(response.length, 37);
-        // assert.equal(typeof response, typeof []);
-        // assert.equal(response[1], statesAndLocalGov[1].state);
-        // assert.equal(response[36], 'Zamfara');
+    // console.log(response[0]);
+    // assert.equal(response.length, 37);
+    // assert.equal(typeof response toequal[]);
+    // assert.equal(response[1].state, statesAndLocalGov[1].state);
+    // assert.equal(response[1].lgas.length, statesAndLocalGov[1].lgas.length);
+  });
+  it("#brands()", function () {
+    var response = laptops.getBrands();
+    // console.log(response)
+    // assert.equal(response.length, 37);
+    // assert.equal(typeof response, typeof []);
+    // assert.equal(response[0], 'Apple');
+  });
+  it("should get laptop details", function () {
+    var response = laptops.getBrand("Hp");
+    console.log(response);
+  });
+  it("#models()", function () {
+    var response = laptops.getModel();
+    // console.log(response);
+    // console.log(response)
+    // assert.equal(response.length, 37);
+    // assert.equal(typeof response, typeof []);
+    // assert.equal(response[1], statesAndLocalGov[1].state);
+    // assert.equal(response[36], 'Zamfara');
+  });
 
-       
-        
-    });
+  // it('#senatorial_districts()', function() {
+  //     var response = naijaStates.senatorial_districts('Lagos');
 
-    // it('#senatorial_districts()', function() {
-    //     var response = naijaStates.senatorial_districts('Lagos');
-       
-    //     assert.equal(response.length, 3);
-    // });
+  //     assert.equal(response.length, 3);
+  // });
 
-    // it('#lgas()', function() {
-    //     var response = naijaStates.lgas('Lagos');
+  // it('#lgas()', function() {
+  //     var response = naijaStates.lgas('Lagos');
 
-    //     assert.equal(response.state, 'Lagos');
-    //     assert.equal(response.lgas.length, 21);
-    // });
+  //     assert.equal(response.state, 'Lagos');
+  //     assert.equal(response.lgas.length, 21);
+  // });
 });
