40feb1450a256ff54fa474a743a6d192cc0e2bf3
Bolaji Olayinka <bolajiolayinka980@gmail.com>
2022-01-07 02:05:24 +0100

    formatted index.js

--- index.js
+++ index.js
@@ -8,34 +8,10 @@ module.exports = {
   getAll: function () {
     return laptopBrands;
   },
-  getBrands: function () {
-    // return laptopBrands.map(function (laptop) {
-    //     return laptop
-    // });
-    return laptopBrands[0]["models"];
-  },
+
   getBrand: function (param) {
     const isBrand = isNaN(param);
     const queryType = isBrand ? "brands" : "models";
-    return laptopBrands.find(function (laptop) {
-      if (laptop[queryType] === param) {
-        return laptop;
-      }
-    });
-  },
-  getModel: function () {
-    return laptopBrands.map(function (laptop) {
-      // Remove first and second array
-      let newArr = [];
-      newArr.push(laptop.models);
-      return newArr.flat();
-      // let newArr = arr.shift();
-      // return newArr.concat.apply();
-    });
-  },
-  getBrand: function (param) {
-    const isBrand = isNaN(param);
-    const queryType = isBrand ? "brands" : "code";
     return laptopBrands.find(function (laptop) {
       if (_lower(laptop[queryType]) === _lower(param)) {
         return laptop;
