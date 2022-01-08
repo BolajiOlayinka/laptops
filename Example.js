1af4401b29b88d8a2427effc46804b3c013b7cdb
Bolaji Olayinka <bolajiolayinka980@gmail.com>
2022-01-08 21:14:49 +0100

    ReadMe in Progress

---
+++ Example.js
@@ -0,0 +1,9 @@
+// ES5
+// const laptops = require('./index');
+
+// ES6
+import laptops from './index';
+
+console.log(laptops.getAll());
+// console.log(laptops.getBrand('all'));
+// console.log(laptops.getBrand("popular"))
